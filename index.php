ec49028cd8e80ae2928bb362b5a380c1303b10b5
Zerik20 <erikaban14@gmail.com>
2024-04-30 09:02:01 -0600

    se cambiaron los iconos en modo escritorio

--- index.php
+++ index.php
@@ -259,80 +259,61 @@
 
 <button class="nav-link active text-black list-club-op" id="v-pills-1-tab" data-bs-toggle="pill" data-bs-target="#v-pills-1" type="button" role="tab" aria-controls="v-pills-1" aria-selected="true">
     <div class="icon-circle">
-        <svg height="24" width="24" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-            <path d="M323.682,323.349h-135.38c-8.138,0-14.738,6.6-14.738,14.73v17.884c0,8.138,6.6,14.738,14.738,14.738h135.38 c8.139,0,14.739-6.6,14.739-14.738v-17.884C338.42,329.949,331.82,323.349,323.682,323.349z"/>
-            <path d="M256,0c-91.181,0-165.1,73.928-165.1,165.1c0,58.685,30.696,110.098,76.829,139.382h176.544 c46.133-29.284,76.829-80.697,76.829-139.382C421.101,73.928,347.181,0,256,0z"/>
-            <polygon points="228.128,440.796 228.128,512 283.871,512 283.871,440.77 310.061,387.979 201.922,387.979"/>
-        </svg>
+        <img src="iconossvg/Golf.svg" alt="Campo de Golf Icon" style="width: 70%; height: 70%;">
     </div>
     <p id="list1acc" class="text-black" data-section="Amenidades" data-value="CampGolfAmeni">Campo de Golf</p>
 </button>
 
-
-
 <button class="nav-link text-black list-club-op w-100 d-flex" id="v-pills-2-tab" data-bs-toggle="pill" data-bs-target="#v-pills-2" type="button" role="tab" aria-controls="v-pills-2" aria-selected="false">
     <div class="icon-circle">
-    <svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 14 14" id="svg2" fill="#000000">
-    <path d="M 3,0 C 2,0 2,0.4670568 2,1 L 2,3 C 2,4 0,4 0,6 l 0,7 c 0,0.566252 0.467057,1 1,1 l 4,0 c 0.599561,0 1,-0.400439 1,-1 L 6,6 C 6,4 4,4 4,3 L 4,1 C 4,0.4337479 4,0 3,0 z m 4,5 c 0,3 0.47106,5 3,5 l 0,2 c 0,0.666911 -1,1 -1.5,1 -1,0 -1.5,0 -1.5,1 l 7,0 C 14,13 13.5,13 12.5,13 12,13 11,12.633602 11,12 l 0,-2 c 2.47106,0 3,-2 3,-5 L 7,5 z" id="alcohol" style="fill:#000000;fill-opacity:1;stroke:none"></path>
-</svg>
+        <img src="iconossvg/Alcohol.svg" alt="Bar Icon" style="width: 60%; height: 60%;">
     </div>
     <p id="list2acc" class="m-0 text-black" data-section="Amenidades" data-value="BarAmeni"> Bar</p>
 </button>
 
 <button class="nav-link text-black list-club-op w-100 d-flex" id="v-pills-3-tab" data-bs-toggle="pill" data-bs-target="#v-pills-3" type="button" role="tab" aria-controls="v-pills-3" aria-selected="false">
     <div class="icon-circle">
-        <svg fill="#000000" viewBox="0 -2.89 122.88 122.88" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="enable-background:new 0 0 122.88 117.09" xml:space="preserve">
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-            <g id="SVGRepo_iconCarrier">
-                <style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style>
-                <g>
-                    <path class="st0" d="M36.82,107.86L35.65,78.4l13.25-0.53c5.66,0.78,11.39,3.61,17.15,6.92l10.29-0.41c4.67,0.1,7.3,4.72,2.89,8 c-3.5,2.79-8.27,2.83-13.17,2.58c-3.37-0.03-3.34,4.5,0.17,4.37c1.22,0.05,2.54-0.29,3.69-0.34c6.09-0.25,11.06-1.61,13.94-6.55 l1.4-3.66l15.01-8.2c7.56-2.83,12.65,4.3,7.23,10.1c-10.77,8.51-21.2,16.27-32.62,22.09c-8.24,5.47-16.7,5.64-25.34,1.01 L36.82,107.86L36.82,107.86z M29.74,62.97h91.9c0.68,0,1.24,0.57,1.24,1.24v5.41c0,0.67-0.56,1.24-1.24,1.24h-91.9 c-0.68,0-1.24-0.56-1.24-1.24v-5.41C28.5,63.53,29.06,62.97,29.74,62.97L29.74,62.97z M79.26,11.23 c25.16,2.01,46.35,23.16,43.22,48.06l-93.57,0C25.82,34.23,47.09,13.05,72.43,11.2V7.14l-4,0c-0.7,0-1.28-0.58-1.28-1.28V1.28 c0-0.7,0.57-1.28,1.28-1.28h14.72c0.7,0,1.28,0.58,1.28,1.28v4.58c0,0.7-0.58,1.28-1.28,1.28h-3.89L79.26,11.23L79.26,11.23 L79.26,11.23z M0,77.39l31.55-1.66l1.4,35.25L1.4,112.63L0,77.39L0,77.39z"></path>
-                </g>
-            </g>
-        </svg>
+        <img src="iconossvg/Restaurant.svg" alt="Restaurante Icon" style="width: 70%; height: 70%;">
     </div>
     <p id="list3acc" class="m-0 text-black" data-section="Amenidades" data-value="RestAmeni"> Restaurante</p>
 </button>
+
 <button class="nav-link text-black list-club-op w-100 d-flex" id="v-pills-3-tab" data-bs-toggle="pill" data-bs-target="#v-pills-6" type="button" role="tab" aria-controls="v-pills-3" aria-selected="false">
     <div class="icon-circle">
-        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M3.15452 1.01195C5.11987 1.32041 7.17569 2.2474 8.72607 3.49603C9.75381 3.17407 10.8558 2.99995 12 2.99995C13.1519 2.99995 14.261 3.17641 15.2946 3.5025C16.882 2.27488 18.8427 1.31337 20.8354 1.01339C21.2596 0.95092 21.7008 1.16534 21.8945 1.55273C22.6719 3.38958 22.6983 5.57987 22.2202 7.49248L22.2128 7.52213C22.0847 8.03536 21.9191 8.69868 21.3876 8.92182C21.7827 9.89315 22 10.9466 22 12.0526C22 14.825 20.8618 17.6774 19.8412 20.2348L19.8412 20.2348L19.7379 20.4936C19.1182 22.0486 17.7316 23.1196 16.125 23.418L13.8549 23.8397C13.1549 23.9697 12.4562 23.7172 12 23.2082C11.5438 23.7172 10.8452 23.9697 10.1452 23.8397L7.87506 23.418C6.26852 23.1196 4.88189 22.0486 4.26214 20.4936L4.15891 20.2348C3.13833 17.6774 2.00004 14.825 2.00004 12.0526C2.00004 10.9466 2.21737 9.89315 2.6125 8.92182C2.08046 8.69845 1.91916 8.05124 1.7909 7.53658L1.7799 7.49248C1.32311 5.66527 1.23531 3.2968 2.10561 1.55273C2.29827 1.16741 2.72906 0.945855 3.15452 1.01195ZM6.58478 4.44052C5.45516 5.10067 4.47474 5.9652 3.71373 6.98132C3.41572 5.76461 3.41236 4.41153 3.67496 3.18754C4.68842 3.48029 5.68018 3.89536 6.58478 4.44052ZM20.2863 6.98133C19.5303 5.97184 18.5577 5.11195 17.4374 4.45347C18.3364 3.9005 19.3043 3.45749 20.3223 3.17455C20.5884 4.40199 20.5853 5.76068 20.2863 6.98133ZM8.85364 5.56694C9.81678 5.20285 10.8797 4.99995 12 4.99995C13.1204 4.99995 14.1833 5.20285 15.1464 5.56694C18.0554 6.66661 20 9.1982 20 12.0526C20 14.4676 18.9891 16.9876 18.0863 19.238L18.0862 19.2382C18.0167 19.4115 17.9478 19.5832 17.8801 19.7531C17.5291 20.6338 16.731 21.2712 15.7597 21.4516L13.4896 21.8733L12.912 20.5896C12.7505 20.2307 12.3935 19.9999 12 19.9999C11.6065 19.9999 11.2496 20.2307 11.0881 20.5896L10.5104 21.8733L8.24033 21.4516C7.26908 21.2712 6.471 20.6338 6.12001 19.7531C6.05237 19.5834 5.98357 19.4119 5.91414 19.2388L5.91395 19.2384L5.91381 19.238C5.01102 16.9876 4.00004 14.4676 4.00004 12.0526C4.00004 9.1982 5.94472 6.66661 8.85364 5.56694ZM10.5 15.9999C10.1212 15.9999 9.77497 16.2139 9.60557 16.5527C9.43618 16.8915 9.47274 17.2969 9.7 17.5999L11.2 19.5999C11.3889 19.8517 11.6852 19.9999 12 19.9999C12.3148 19.9999 12.6111 19.8517 12.8 19.5999L14.3 17.5999C14.5273 17.2969 14.5638 16.8915 14.3944 16.5527C14.225 16.2139 13.8788 15.9999 13.5 15.9999H10.5ZM9.62134 11.1212C9.62134 11.9497 8.94977 12.6212 8.12134 12.6212C7.29291 12.6212 6.62134 11.9497 6.62134 11.1212C6.62134 10.2928 7.29291 9.62125 8.12134 9.62125C8.94977 9.62125 9.62134 10.2928 9.62134 11.1212ZM16 12.4999C16.8284 12.4999 17.5 11.8284 17.5 10.9999C17.5 10.1715 16.8284 9.49994 16 9.49994C15.1716 9.49994 14.5 10.1715 14.5 10.9999C14.5 11.8284 15.1716 12.4999 16 12.4999Z" fill="#000000"></path> </g></svg>
+        <img src="iconossvg/Dog.svg" alt="Parque de mascotas Icon" style="width: 70%; height: 70%;">
     </div>
     <p id="list6acc" class="m-0 text-black" data-section="Amenidades" data-value="PetParkAmeni"> Parque de mascotas</p>
 </button>
-                    <button class="nav-link text-black list-club-op w-100 d-flex" id="v-pills-7-tab" data-bs-toggle="pill" data-bs-target="#v-pills-7" type="button" role="tab" aria-controls="v-pills-7" aria-selected="false"><i class="fas fa-chevron-circle-right mt-1 me-1" aria-hidden="true"></i><p id="list7acc" class="m-0 text-black" data-section="Amenidades" data-value="SpaAmeni"> Spa</p></button>
-                    <button class="nav-link text-black list-club-op w-100 d-flex" id="v-pills-8-tab" data-bs-toggle="pill" data-bs-target="#v-pills-8" type="button" role="tab" aria-controls="v-pills-8" aria-selected="false"><i class="fas fa-chevron-circle-right mt-1 me-1" aria-hidden="true"></i><p id="list8acc" class="m-0 text-black" data-section="Amenidades" data-value="PisciAmeni"> Piscina</p></button>
-                    <!--
-                    <button class="nav-link text-start list-club-op w-100 d-flex" id="v-pills-9-tab" data-bs-toggle="pill" data-bs-target="#v-pills-9" type="button" role="tab" aria-controls="v-pills-9" aria-selected="false"><i class="fas fa-chevron-circle-right mt-1 me-1" aria-hidden="true"></i><p id="list9acc" class="m-0 text-start"> Lockers</p></button>
--->
-                </div>
-                <div class="col-md-4 cont-lista2"  data-aos="fade-up">
+
+<button class="nav-link text-black list-club-op w-100 d-flex" id="v-pills-7-tab" data-bs-toggle="pill" data-bs-target="#v-pills-7" type="button" role="tab" aria-controls="v-pills-7" aria-selected="false">
+    <div class="icon-circle">
+        <img src="iconossvg/Spa.svg" alt="Spa Icon" style="width: 70%; height: 70%;">
+    </div>
+    <p id="list7acc" class="m-0 text-black" data-section="Amenidades" data-value="SpaAmeni"> Spa</p>
+</button>
+
+<button class="nav-link text-black list-club-op w-100 d-flex" id="v-pills-8-tab" data-bs-toggle="pill" data-bs-target="#v-pills-8" type="button" role="tab" aria-controls="v-pills-8" aria-selected="false">
+    <div class="icon-circle">
+        <img src="iconossvg/Piscina.svg" alt="Piscina Icon" style="width: 70%; height: 70%;">
+    </div>
+    <p id="list8acc" class="m-0 text-black" data-section="Amenidades" data-value="PisciAmeni"> Piscina</p>
+</button>
+</div>
+<div class="col-md-4 cont-lista2"  data-aos="fade-up">
                     
                   
-                </div>
+</div>
                 
-            </div>
-            <div class="nav row nav-pills me-3 btn-list2-movil" id="v-pills-tab"  data-aos="fade-up" role="tablist" aria-orientation="horizontal" style="display:none">
-                <div class="row cont-lista2">
+    </div>
+        <div class="nav row nav-pills me-3 btn-list2-movil" id="v-pills-tab"  data-aos="fade-up" role="tablist" aria-orientation="horizontal" style="display:none">
+            <div class="row cont-lista2">
                     <button class="nav-link active text-start list-club-op w-100 d-flex p-2 align-items-center" id="v-pills-1-tab" data-bs-toggle="pill" data-bs-target="#v-pills-1" type="button" role="tab" aria-controls="v-pills-1" aria-selected="true"><i class="fas fa-chevron-circle-right" aria-hidden="true"></i> <p id="list1accm" class="m-0 text-black" data-section="Amenidades" data-value="CampGolfAmeni"> Campo de Golf</p></button>
                     <button class="nav-link text-start list-club-op w-100 d-flex p-2 align-items-center" id="v-pills-2-tab" data-bs-toggle="pill" data-bs-target="#v-pills-2" type="button" role="tab" aria-controls="v-pills-2" aria-selected="false"><i class="fas fa-chevron-circle-right" aria-hidden="true"></i> <p id="list2accm" class="m-0 text-black" data-section="Amenidades" data-value="BarAmeni"> Bar</p></button>
                     <button class="nav-link text-black list-club-op w-100 d-flex p-2 align-items-center" id="v-pills-3-tab" data-bs-toggle="pill" data-bs-target="#v-pills-3" type="button" role="tab" aria-controls="v-pills-3" aria-selected="false"><i class="fas fa-chevron-circle-right" aria-hidden="true"></i> <p id="list3accm" class="m-0 text-black" data-section="Amenidades" data-value="RestAmeni"> Restaurante</p></button>
-                    <!--
-                    <button class="nav-link text-start list-club-op w-100 d-flex p-2 align-items-center" id="v-pills-4-tab" data-bs-toggle="pill" data-bs-target="#v-pills-4" type="button" role="tab" aria-controls="v-pills-4" aria-selected="false"><i class="fas fa-chevron-circle-right" aria-hidden="true"></i> <p id="list4accm" class="m-0 text-start"> lago</p></button>
-                    <button class="nav-link text-start list-club-op w-100 d-flex p-2 align-items-center" id="v-pills-5-tab" data-bs-toggle="pill" data-bs-target="#v-pills-5" type="button" role="tab" aria-controls="v-pills-5" aria-selected="false"><i class="fas fa-chevron-circle-right" aria-hidden="true"></i> <p id="list5accm" class="m-0 text-start"> Canal de nado</p></button>
--->
                     <button class="nav-link text-black list-club-op w-100 d-flex p-2 align-items-center" id="v-pills-6-tab" data-bs-toggle="pill" data-bs-target="#v-pills-6" type="button" role="tab" aria-controls="v-pills-6" aria-selected="false"><i class="fas fa-chevron-circle-right" aria-hidden="true"></i> <p id="list6accm" class="m-0 text-black" data-section="Amenidades" data-value="PetParkAmeni"> Parque de mascotas</p></button>
                     <button class="nav-link text-black list-club-op w-100 d-flex p-2 align-items-center" id="v-pills-7-tab" data-bs-toggle="pill" data-bs-target="#v-pills-7" type="button" role="tab" aria-controls="v-pills-7" aria-selected="false"><i class="fas fa-chevron-circle-right" aria-hidden="true"></i> <p id="list7accm" class="m-0 text-black" data-section="Amenidades" data-value="SpaAmeni"> Spa</p></button>
                     <button class="nav-link text-start list-club-op w-100 d-flex p-2 align-items-center" id="v-pills-8-tab" data-bs-toggle="pill" data-bs-target="#v-pills-8" type="button" role="tab" aria-controls="v-pills-8" aria-selected="false"><i class="fas fa-chevron-circle-right" aria-hidden="true"></i> <p id="list8accm" class="m-0 text-black" data-section="Amenidades" data-value="PisciAmeni"> Piscina</p></button>
-                    <!--
-                    <button class="nav-link text-start list-club-op w-100 d-flex p-2 align-items-center" id="v-pills-9-tab" data-bs-toggle="pill" data-bs-target="#v-pills-9" type="button" role="tab" aria-controls="v-pills-9" aria-selected="false"><i class="fas fa-chevron-circle-right" aria-hidden="true"></i> <p id="list9accm" class="m-0 text-start"> Lockers</p></button>
--->
-                    <!--button class="nav-link text-start list-club-op w-100 d-flex p-2 align-items-center" id="v-pills-10-tab" data-bs-toggle="pill" data-bs-target="#v-pills-10" type="button" role="tab" aria-controls="v-pills-10" aria-selected="false"><i class="fas fa-chevron-circle-right" aria-hidden="true"></i> <p id="list10accm" class="m-0 text-start"> Campo de practica</p></!--button>
-                    <button class="nav-link text-start list-club-op w-100 d-flex p-2 align-items-center" id="v-pills-11-tab" data-bs-toggle="pill" data-bs-target="#v-pills-11" type="button" role="tab" aria-controls="v-pills-11" aria-selected="false"><i class="fas fa-chevron-circle-right" aria-hidden="true"></i> <p id="list11accm" class="m-0 text-start"> Área de baños</p></button>
-                    <button-- class="nav-link text-start list-club-op w-100 d-flex p-2 align-items-center" id="v-pills-12-tab" data-bs-toggle="pill" data-bs-target="#v-pills-12" type="button" role="tab" aria-controls="v-pills-12" aria-selected="false"><i class="fas fa-chevron-circle-right" aria-hidden="true"></i> <p id="list12accm" class="m-0 text-start"> Pro Shop Golf</p></button-->
-                </div>
-                
-                
+                </div>                                
             </div>
         </div>
         <div class="col-md-7 p-0">
