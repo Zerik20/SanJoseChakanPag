f6654511d89b80c54c235203d9c872ff2a8f8101
zerik20 <erikaban14@gmail.com>
2024-05-09 14:44:24 -0600

    se agrego el kids club

--- emociones.php
+++ emociones.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Royal County San José Chakan | Boletín</title>
+    <title>Royal County Chocholá | Boletín</title>
     <?php
     include('templates/link.php');
     ?>
@@ -26,25 +26,120 @@
 
 </div>
 
-<div class="container cont1-boletin my-5 py-5">
+<div id="emo-cont-1" class="container cont1-boletin my-5 py-5">
     <div class="p-3"></div>
-    <h1 id="actit" class="tit text-center" data-aos="fade-up" style="color: brown;">KIDS CLUB</h1>
-    <!--<h1 id="actit" class="tit text-center" data-aos="fade-up">PROXIMAMENTE</h1>-->
+    
+    <h4 id="subtitac" class="subtitle text-center">KiDS Club</h4>
+    <h1 id="titac" class="tit text-center mb-5">“Mayan Kingdom”</h1>
+    
     <div class="p-3"></div>
-    <div class="row justify-content-center">
-        <div class="col-md-8 text-center">
-            <div class="image-relax" style="background-image: url('img/ninos.jpg'); background-size: cover; background-position: center; height: 400px;"></div>
+
+    <div class="row w-100 m-0 p-0 d-flex">
+        <div class="col-md-6 row acerca-img">
+            <div class="col-md-6 p-2">
+                <div class="image-acerca" style="background-image: url('img/pg.jpg') !important; margin-top: -20px;"></div>
+            </div>
+            <div class="col-md-6 p-2">
+                <div class="image-acerca" style="background-image: url('img/pb.jpg') !important; margin-top: 20px;"></div>
+            </div>
         </div>
-    </div>
-    <!--
-    <div class="row justify-content-center mt-4">  Agregamos un margen top de 4 unidades 
-        <div class="col-md-8 text-center">
-            <div class="image-relax" style="background-image: url('img/res2.jpeg'); background-size: cover; background-position: center; height: 400px;"></div>
+        <div class="col-md-6 d-flex align-items-center p-5 acerca-info">
+            <div data-aos="fade-up">
+                <!--h4 id="subtitac" class="subtitle ">Eternal & Spa</!--h4-->
+                <h2 id="titac" class="tit ">Once upon a time</h2>
+                <p id="txtac" class="">Once upon a time in the Mayan kingdom In Chochola, An amazing pirate girl and a brave sailor boy found a world of adventure in an abandoned Mayan palace....</p>
+                <!--a id="btnac" href="productos.php" class="boton">Ver Más <i class="fas fa-arrow-right"></i></!--a-->
+            </div>
         </div>
+        
     </div>
--->
+    
 </div>
 
+<div class="p-5 m-5"></div>
+
+<div class="container mt-5 pt-5">
+    <div class="row w-100 m-0 p-0 d-flex">
+        
+        <div class="col-md-6 d-flex align-items-center p-5 acerca-info">
+            <div data-aos="fade-up">
+                <!--h4 id="subtitac" class="subtitle ">Eternal & Spa</!--h4-->
+                <h2 id="titac" class="tit ">The Cave Of The "Maya Bee"</h2>
+                <p id="txtac" class="">The city of the Queen Bee. <br>
+                    It is a calm and stimulating environment for children from 6 months to 7 years, for wonderful daydreams and magical creativity while being cared for by our attentive Ambassadors. This educational hive takes a fun approach to imparting the importance of pollinators. Here, children learn all about the secret life of bees by becoming one. Floor-to-ceiling immersion within a honeycomb environment where children can play as little bees under an interactive weather-themed roof. A dream world, with a harmonious mix of fun gameplay and instructive discovery.</p>
+                <!--a id="btnac" href="productos.php" class="boton">Ver Más <i class="fas fa-arrow-right"></i></!--a-->
+            </div>
+        </div>
+        <div class="col-md-6 row acerca-img">
+            <div class="col-md-8 p-2">
+                <div class="image-acerca" style="background-image: url('img/abeja.jpg') !important; "></div>
+            </div>
+            <div class="col-md-12">
+                <div class="image-acerca" style="background-image: url('img/niñoss.jpg') !important; margin-right: -52px; margin-left: 98px; margin-top: -94px;"></div>
+            </div>
+            
+        </div>
+        
+    </div>
+</div>
+
+
+<div class="p-5 m-5"></div>
+
+
+<div class="container">
+    <div class="row w-100 m-0 p-0 d-flex">
+        <div class="col-md-7 row acerca-img">
+            <div class="col-md-6 p-2">
+                <div class="image-acerca" style="background-image: url('img/pastel.jpg') !important; margin-top: -20px;"></div>
+            </div>
+            <div class="col-md-6 p-2">
+                <div class="image-acerca" style="background-image: url('img/piñata.jpg') !important; margin-top: 20px;"></div>
+            </div>
+        </div>
+        <div class="col-md-5 d-flex align-items-center p-5 acerca-info">
+            <div data-aos="fade-up">
+                <!--h4 id="subtitac" class="subtitle ">Eternal & Spa</!--h4-->
+                <h2 id="titac" class="tit ">Special Celebrations</h2>
+                <p id="txtac" class="">The magic of Christmas and the New Year, the frantic anticipation of birthdays, the arrival of spring full of flowers, Hanal Pixán and Easter. “Mayan Kingdom” celebrates every occasion. Just as one set of decorations is removed, another one rises up, creating one fantastical world after another. Smiles, laughter and unforgettable memories shining on sweet little faces.</p>
+                <!--a id="btnac" href="productos.php" class="boton">Ver Más <i class="fas fa-arrow-right"></i></!--a-->
+            </div>
+        </div>
+        
+    </div>
+</div>
+
+<div class="p-5 m-5"></div>
+
+
+<div class="container mt-5 pt-5">
+    <div class="row w-100 m-0 p-0 d-flex">
+        
+        <div class="col-md-6 d-flex align-items-center p-5 acerca-info">
+            <div data-aos="fade-up">
+                <!--h4 id="subtitac" class="subtitle ">Eternal & Spa</!--h4-->
+                <h2 id="titac" class="tit ">Picture Postcard Smiles</h2>
+                <p id="txtac" class="">The alchemists of "Mayan Kingdom" imagine endless activities and experiences so that all ages can come together in an unforgettable moment.
+                    <br><br>
+                    Guided tours of the treasure, introductory workshops, where a multitude of extraordinary experiences designed for families are revealed while living a dream life in “Royal County Chocholá”.
+                </p>
+                <!--a id="btnac" href="productos.php" class="boton">Ver Más <i class="fas fa-arrow-right"></i></!--a-->
+            </div>
+        </div>
+        <div class="col-md-6 row acerca-img d-flex justify-content-end">
+            <div class="col-md-12 p-3">
+                <div class="image-acerca" style="background-image: url('img/fam1.jpg') !important; margin-top: -20px;"></div>
+            </div>
+            <div class="col-md-12 p-3" style="margin-right: -25px;">
+                <div class="image-acerca" style="background-image: url('img/fam2.jpg') !important; margin-top: -20px;"></div>
+            </div>
+        </div>
+        
+    </div>
+</div>
+
+
+<div class="p-5 m-5"></div>
 
 
 
