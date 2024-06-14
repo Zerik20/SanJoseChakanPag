060cf1507828df89e46eef87cd5cf5d640e2d283
Zerik20 <erikaban14@gmail.com>
2024-06-14 09:46:55 -0600

    Se agrego y subio una imagen de promocion

--- artes-culinarias.php
+++ artes-culinarias.php
@@ -52,6 +52,18 @@
         transition-duration: 0.4s;
         cursor: pointer;
         border-radius: 10px;">Visita el menú del día</button>
+        <button data-section="CuliArt" data-value="BotonCuliArtPro" onclick="window.open('img/DiaDelPadre.png', '_blank')" style="background-color: brown; /* Green */
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 30px 2px;
+        transition-duration: 0.4s;
+        cursor: pointer;
+        border-radius: 10px;">Promoción</button>
 </div>
 
         </div>
